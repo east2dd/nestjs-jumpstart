2835e57369f55ef5b689ba3a2ec9e55f83d8136d
xyingsoft <xingxing2d@gmail.com>
2020-06-23 21:29:30 +1000

    Change swagger server url variable

--- src/swagger.ts
+++ src/swagger.ts
@@ -3,7 +3,7 @@ import { DocumentBuilder, SwaggerModule, OpenAPIObject } from '@nestjs/swagger'
 import { configService } from './config/config.service';
 
 export function initSwagger(app: INestApplication): OpenAPIObject {
-  const appUrl = configService.getValue("APP_URL", "http://localhost:8080")
+  const appUrl = configService.getValue("API_URL", "http://localhost:8080")
   const config = new DocumentBuilder()
     .setTitle('API Document')
     .setDescription('API Document')
